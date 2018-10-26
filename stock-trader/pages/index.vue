e1541a426743400bb8a6e9f96f8e19deb80e7e6f
Alex Avila <alex.avilx@icloud.com>
2018-10-26 16:16:19 -0700

    built home page content (needs vuex data)

--- stock-trader/pages/index.vue
+++ stock-trader/pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <section>
-    <div/>
+  <section class="section">
+    <div class="container box">
+      <div>
+        <h1
+          class="title"
+          style="margin: 0 0 1rem"
+        >
+          Trade or View Your Portfolio
+        </h1>
+        <p>You may save and load your data.</p>
+        <p>Click on "End Day" to begin a new Day!</p>
+      </div>
+      <hr style="margin: 1rem 0 1rem">
+      <p class="has-text-weight-bold is-size-5">Your Funds: ${{ funds }}</p>
+    </div>
   </section>
 </template>
